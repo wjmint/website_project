e4f84b590552a9f8de7dd63d669f2e1aa7c5ab76
wjmint <jamie060529@gmail.com>
2021-04-10 13:31:20 +0900

    made changes in html and models

--- django_own/mysite/catalog/views.py
+++ django_own/mysite/catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from catalog.models import Cat, CatSpecies, CatColour   
 
 # Create your views here.
+def index(request):
+    # view function for the home page of the site
+
+    # Generate counts of some of the main objects
+    num_cats = Cat.objects.all().count()
+
+    context = {
+        'num_cats': num_cats
+    }
+
+    return render(request, 'index.html', context=context)
