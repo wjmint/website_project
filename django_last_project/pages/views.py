38783f7a4186914a308004c4e578498b163479cf
wjmint <jamie060529@gmail.com>
2021-04-17 15:02:10 +0900

    IT'S WORKINGGGG

--- django_last_project/pages/views.py
+++ django_last_project/pages/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    html = '<h1>Hello World</h1>'
-    return HttpResponse(html)
+    # html = '<h1>Hello World</h1>'
+    # return HttpResponse(html)
+    
+    return render(request, 'pages/index.html')
