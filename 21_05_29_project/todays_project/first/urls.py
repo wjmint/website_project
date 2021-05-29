4a81681c8524454758e495dff9c54998d87c61e6
wjmint <jamie060529@gmail.com>
2021-05-29 15:04:59 +0900

    25/05/29

--- 21_05_29_project/todays_project/first/urls.py
+++ 21_05_29_project/todays_project/first/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/', views.User, name='user'),
 ]
 
 
--- 21_05_29_project/todays_project/first/views.py
+++ 21_05_29_project/todays_project/first/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from . import forms
+from .forms import User
 
 # Create your views here.
 def index(request):
     return render(request, 'first/index.html')
+
+def User(request):
+    form = forms.User()
+    if request.method =='POST':
+        form = forms.User(request.POST)
+
+        if form.is_valid():
+            print('First Name:', form.cleaned_data['first_name'])
+            print('Last Name:', form.cleaned_data['last_name'])
+            print('E-mail:', form.cleaned_data['email'])
+
+
+    return render(request, 'first/form.html', {'form': form})
