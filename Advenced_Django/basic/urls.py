882ab090a8d35e0a4be9636b47e9d9a6c9a94603
wjmint <jamie060529@gmail.com>
2021-09-11 15:06:04 +0900

    making delete button, not finished

--- Advenced_Django/basic/urls.py
+++ Advenced_Django/basic/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.conf.urls import url    
 from django.urls import path
-from .views import SchoolListView, SchoolDetailView
+from .views import SchoolDetailDeleteView, SchoolDetailUpdateView, SchoolListView, SchoolDetailView
 
 
 app_name = 'basic_app'
 urlpatterns = [
     path('', SchoolListView.as_view(), name='list'),
     url(r'(?P<pk>[-\w]+)/',SchoolDetailView.as_view(),name='detail'),
+    url(r'^update(?P<pk>\d+)/$', SchoolDetailUpdateView.as_view(), name='update'),
+    # url(r'^delete(?P<pk>\d+)/$', SchoolDetailDeleteView.as_view(), name='delete')
     
 ]
--- Advenced_Django/basic/views.py
+++ Advenced_Django/basic/views.py
@@ -1,5 +1,6 @@
+from django.db.models import fields
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, UpdateView, DeleteView, CreateView
 from . import models
 
 # Create your views here.
@@ -13,5 +14,17 @@ class SchoolListView(ListView):
     
 class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
-    model = models.School
+    model = models.School   
     template_name = 'basic_app/school_detail.html'
+
+class SchoolDetailUpdateView(UpdateView):
+    fields = ('name', 'principals')
+    model = models.School
+
+class SchoolDetailCreateView(CreateView):
+    fields = ('name', 'principals', 'location')
+    model = models.School
+
+class SchoolDetailDeleteView(DeleteView):
+    fields = ('name', 'principals')
+    model = models.School
